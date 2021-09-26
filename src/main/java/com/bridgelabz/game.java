e412173c56810e740b7de9cbdf1ab2a563b8363b
ankushmalode <you@example.com>
2021-09-26 19:57:33 +0530

    UC4

--- src/main/java/com/bridgelabz/game.java
+++ src/main/java/com/bridgelabz/game.java
@@ -3,19 +3,24 @@ package com.bridgelabz;
 public class game {
     public static void main(String[] args) {
         int snake=1, ladder=2, player=0;
-        int dice=(int)(Math.random()*6+1);
-        System.out.println(dice);
+        while (player<100) {
+            int dice=(int)(Math.random()*6+1);
+//            System.out.println(dice);
 
-        int check=(int)(Math.random()*3+1);
-        if (check==snake) {
-            player= player+dice;
+            int check=(int)(Math.random()*3+1);
+            if (check==ladder) {
+                player= player+dice;
+            }
+            else if (check==snake) {
+                player=player-dice;
+                if (player<0) {
+                    player=0;
+                }
+            }
+            else {
+                player=player+dice;
+            }
+            System.out.println("Player current position is: "+player);
         }
-        else if (check==ladder) {
-            player=player-dice;
-        }
-        else {
-            player=player+dice;
-        }
-        System.out.println("Player position is: "+player);
     }
 }
