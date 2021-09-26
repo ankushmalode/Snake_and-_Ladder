faf99bfa7465290e31b268bf5fc4104b518ac6ad
ankushmalode <you@example.com>
2021-09-26 17:54:27 +0530

    UC3

--- src/main/java/com/bridgelabz/game.java
+++ src/main/java/com/bridgelabz/game.java
@@ -1,11 +1,21 @@
 package com.bridgelabz;
 
-import java.util.Random;
-
 public class game {
     public static void main(String[] args) {
-        int snake=0, ladder=0;
+        int snake=1, ladder=2, player=0;
         int dice=(int)(Math.random()*6+1);
         System.out.println(dice);
+
+        int check=(int)(Math.random()*3+1);
+        if (check==snake) {
+            player= player+dice;
+        }
+        else if (check==ladder) {
+            player=player-dice;
+        }
+        else {
+            player=player+dice;
+        }
+        System.out.println("Player position is: "+player);
     }
 }
