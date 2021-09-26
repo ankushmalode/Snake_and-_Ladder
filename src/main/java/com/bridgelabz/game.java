c96c2701d372cf39713e9359f59a0a9d85e1187e
ankushmalode <you@example.com>
2021-09-26 17:28:03 +0530

    second commit uc2

--- src/main/java/com/bridgelabz/game.java
+++ src/main/java/com/bridgelabz/game.java
@@ -1,7 +1,11 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class game {
     public static void main(String[] args) {
         int snake=0, ladder=0;
+        int dice=(int)(Math.random()*6+1);
+        System.out.println(dice);
     }
 }
