4a3076712404359f5dfe940478f2be360a8daaca
ankushmalode <you@example.com>
2021-09-26 23:22:58 +0530

    UC7

--- src/main/java/com/bridgelabz/game.java
+++ src/main/java/com/bridgelabz/game.java
@@ -1,13 +1,13 @@
 package com.bridgelabz;
-
 public class game {
     public static void main(String[] args) {
         int snake=1, ladder=2, player=0, count=0;
-        while (player<100) {
-            int dice=(int)(Math.random()*6+1);
-//            System.out.println(dice);
+        int player1=0, player2=0;
+        while (player1<100 && player2<100) {
 
+            int dice=(int)(Math.random()*6+1);
             int check=(int)(Math.random()*3+1);
+
             if (check==ladder) {
                 player= player+dice;
                 if (player>100)
@@ -20,12 +20,24 @@ public class game {
                 else if (player>100)
                     player=player-dice;
             }
+            if (count%2==0) {
+                player1 = player;
+                System.out.println("Player1 current position is: " +player1);
+            }
             else {
-                player=player+0;
+                player2 = player;
+                System.out.println("Player2 current position is: "+player2);
             }
             count++;
-            System.out.println("Player current position is: "+player);
+//          System.out.println("Player current position is: "+player);
         }
-        System.out.println(count+" times the dice was played.");
+        System.out.println("\n"+count+" times the dice was played.\n");
+//        System.out.println(player1+" position");
+//        System.out.println(player2+" position.");
+
+        if (player1>player2)
+            System.out.println("The winner of this game is Player1");
+        else
+            System.out.println("The winner of this game is Player2");
     }
 }
